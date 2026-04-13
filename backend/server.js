8dad90e33d0c8fba9ea9dce0960c078b3c4d85a8
Vijayadass M <Vijaydass0702@gmail.com>
2026-04-13 11:08:37 +0530

    Added root route

--- backend/server.js
+++ backend/server.js
@@ -33,3 +33,6 @@ app.use("/api/bookings", bookingRoutes);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT} 🚀`);
 });
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
